7712db1cda4c098336e12b7be2cf33de12f55e5d
Ayumu <a.satou.sys23@morijyobi.ac.jp>
2023-11-09 14:59:06 +0900

    BMI計算機

--- Main.java
+++ Main.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double n = sc.nextDouble();
-        double x = sc.nextDouble();
-        double y = x / (n * n);
-        System.out.println(String.format("%.2f", y));
+        for (int i = 0; i < 2; i++) {
+            System.out.println("身長を入力してください(メートル表記)");
+            double n = sc.nextDouble();
+            System.out.println("体重を入力してください");
+            double x = sc.nextDouble();
+            double y = x / (n * n);
+            System.out.println("あなたのBMIは" + String.format("%.2f", y) + "です");
+        }
     }
 }
